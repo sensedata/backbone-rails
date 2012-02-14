6077a5328eeae93441001382cb08815e4dca9025
Yuri Gadow <yuri@gadow.name>
2012-02-14 10:25:23 -0700

    Refactor most datalink logic to be outside of loop covering input elements in given section or form.

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -3,91 +3,104 @@
 
   return $.extend($.fn, {
     backboneLink: function (model) {
-      return $(this).find(":input").each(function () {
-        var copyFormToModel, copyModelToForm, el, elIsEqualTo, name;
 
-        el = $(this);
-        name = el.attr("name");
+      var copyFormToModel, copyModelToForm, el, elIsEqualTo, handleInputChange;
 
-        if (_.isUndefined(name) || _.isEmpty(name)) {
-          return;
+      copyFormToModel = function (el) {
+        var attrs, checked, modelValue, propertyName;
+
+        attrs = {};
+        propertyName = el.attr("name");
+        modelValue = model.get(propertyName);
+
+        if (el.is(":checkbox")) {
+          checked = el.is(":checked");
+
+          if ($.isArray(modelValue)) {
+            attrs[propertyName] = (checked) ? modelValue.slice().concat([el.val()]) : _.without(modelValue, el.val());
+
+          } else if (/^(true|on|yes|1)$/i.test(el.val())) {
+            attrs[propertyName] = checked;
+
+          } else if (/^(false|off|no|0)$/i.test(el.val())) {
+            attrs[propertyName] = !checked;
+
+          } else {
+            attrs[propertyName] = (checked) ? el.val() : null;
+          }
+
+        } else if (el.is(":radio")) {
+          if (el.is(":checked")) {
+            attrs[propertyName] = el.val();
+
+          } else if (elIsEqualTo(el, modelValue)) {
+            attrs[propertyName] = null;
+          }
+
+        } else {
+          attrs[propertyName] = (/^(\s*|null)$/.test(el.val())) ? null : el.val();
         }
 
-        elIsEqualTo = function (modelValue) {
-          if (modelValue === el.val()) {
-            return true;
-          } else if (modelValue === undefined) {
-            
-          }
-          return (modelValue === el.val()) || (modelValue !== undefined && modelValue.toString() === el.val());
-        };
+        return model.set(attrs);
+      };
 
-        copyModelToForm = function () {
-          var modelValue;
-
-          modelValue = model.get(name);
-          if (el.is(":checkbox")) {
-            if (($.isArray(modelValue) && modelValue.indexOf(el.val()) >= 0) || elIsEqualTo(modelValue)) {
-              el.attr("checked", "checked");
-            } else {
-              el.removeAttr("checked");
-            }
-
-          } else if (el.is(":radio")) {
-            if (elIsEqualTo(modelValue)) {
-              el.attr("checked", "checked");
-            }
+      copyModelToForm = function (el, modelValue) {
+        if (el.is(":checkbox")) {
+          if (($.isArray(modelValue) && modelValue.indexOf(el.val()) >= 0) || elIsEqualTo(el, modelValue)) {
+            el.attr("checked", "checked");
           } else {
-            if (modelValue === null) {
-              el.val("");
-            } else {
-              el.val(modelValue);
-            }
+            el.removeAttr("checked");
           }
 
+        } else if (el.is(":radio")) {
+          if (elIsEqualTo(el, modelValue)) {
+            el.attr("checked", "checked");
+          }
+
+        } else {
+          if (modelValue === null) {
+            el.val("");
+          } else {
+            el.val(modelValue);
+          }
+        }
+
+        return el;
+      };
+
+      elIsEqualTo = function (el, modelValue) {
+        var elValue;
+
+        elValue = el.val();
+
+        if (modelValue === undefined || modelValue === null) {
+          return (elValue === null || elValue.length === 0);
+        }
+
+        return (modelValue === elValue) || (modelValue.toString() === elValue);
+      };
+
+      handleInputChange = function (e) {
+        copyFormToModel($(e.target));
+      };
+
+      return $(this).find(":input").each(function () {
+        var el, propertyName;
+
+        el = $(this);
+        propertyName = el.attr("name");
+
+        // Skip unnamed inputs
+        if (_.isUndefined(propertyName) || _.isEmpty(propertyName)) {
           return el;
-        };
+        }
 
-        copyFormToModel = function () {
-          var attrs, checked, el, modelValue;
-          attrs = {};
-          el = $(this);
-          modelValue = model.get(name);
+        el.bind("change", handleInputChange);
 
-          if (el.is(":checkbox")) {
-            checked = el.is(":checked");
-
-            if ($.isArray(modelValue)) {
-              attrs[name] = (checked) ? modelValue.slice().concat([el.val()]) : _.without(modelValue, el.val());
-
-            } else if (/^(true|on|yes|1)$/i.test(el.val())) {
-              attrs[name] = checked;
-
-            } else if (/^(false|off|no|0)$/i.test(el.val())) {
-              attrs[name] = !checked;
-
-            } else {
-              attrs[name] = (checked) ? el.val() : null;
-            }
-
-          } else if (el.is(":radio")) {
-            if (el.is(":checked")) {
-              attrs[name] = el.val();
-
-            } else if (elIsEqualTo(modelValue)) {
-              attrs[name] = null;
-            }
-
-          } else {
-            attrs[name] = (/^(\s*|null)$/.test(el.val())) ? null : el.val();
-          }
-
-          return model.set(attrs);
-        };
-
-        copyModelToForm();
-        model.bind("change:" + name, copyModelToForm);
-        el.bind("change", copyFormToModel);
+        copyModelToForm(el, model.get(propertyName));
+        model.bind("change:" + propertyName, function (model, modelValue) {
+          copyModelToForm(el, modelValue);
+        });
 
         return el;
       });
